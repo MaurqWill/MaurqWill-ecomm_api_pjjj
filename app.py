799e1c1411e2fa9b1f0aa48ecf49edfd9d2d4759
MaurqWill <mwilliams.mw74@gmail.com>
2024-08-23 13:01:14 -0400

    app.py and config change

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from database import db
 from models.schemas import ma
 # from limiter import limiter
@@ -28,8 +29,12 @@ def create_app(config_name):
     ma.init_app(app)
     # limiter.init_app(app)
     cache.init_app(app)
+    CORS(app)
 
     print('Running')
+    blueprint_config(app)
+    # rate_limit_config()
+
     return app
 
 def blueprint_config(app):
@@ -41,15 +46,15 @@ def blueprint_config(app):
 # def rate_limit_config():
 #     limiter.limit("200 per day")
 
-if __name__ == '__main__':
-    app = create_app('DevelopmentConfig')
+# if __name__ == '__main__':
+app = create_app('ProductionConfig')
 
-    blueprint_config(app)
+    # blueprint_config(app)
 
-    # rate_limit_config()
+    # # rate_limit_config()
 
-    with app.app_context():
-        # db.drop_all()
+with app.app_context():
+        db.drop_all()
         db.create_all()
 
-    app.run()
+    # app.run()
